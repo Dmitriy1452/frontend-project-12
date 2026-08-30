fbdc577995e664e38e5fd0a13aa426767c3cd845
Dmitriy <tolstyachokhenkov@yandex.ru>
2026-08-30 18:18:08 +0300

    fixing errors

--- code/src/init.js
+++ code/src/init.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { store } from '../../frontend/src/store';
 import i18n from '../../frontend/src/i18n';
 import App from '../../frontend/src/App';
-import '../../frontend/src/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../frontend/src/App.css';
 
 const init = (socket) => {
-  const container = document.createElement('div');
-  container.id = 'root';
-  document.body.appendChild(container);
-
-  const root = createRoot(container);
-  root.render(
+  return (
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <BrowserRouter>
@@ -24,18 +18,6 @@ const init = (socket) => {
       </I18nextProvider>
     </Provider>
   );
-
-  return {
-    container,
-    root,
-    cleanup: () => {
-      root.unmount();
-      if (container.parentNode) {
-        container.parentNode.removeChild(container);
-      }
-    },
-    getContainer: () => container,
-  };
 };
 
 export default init;
