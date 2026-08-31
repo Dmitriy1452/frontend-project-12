6b2fe3bf1cd23e22d94ce15d77b6280a969b1354
Dmitriy <tolstyachokhenkov@yandex.ru>
2026-08-31 21:17:54 +0300

    modified vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'] }, 
   plugins: [react()],
   server: {
     port: 5002,
