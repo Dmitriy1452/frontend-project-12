eae07b650e65c1ccc4f649917d123c25815ae04f
Dmitriy <tolstyachokhenkov@yandex.ru>
2026-09-01 20:59:15 +0300

    fixing errors

--- src/init.js
+++ src/init.js
@@ -1,2 +1,23 @@
-import init from '../frontend/src/init'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import { store } from '../frontend/src/store';
+import i18n from '../frontend/src/i18n';
+import App from '../frontend/src/App';
+import '../frontend/src/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const init = async (socket) => {
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18nextProvider>
+    </Provider>
+  );
+};
+
 export default init;
